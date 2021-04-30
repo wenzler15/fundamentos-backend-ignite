3f64707d52956858b6b0780b3dbd34671a489507
dougW <victor.oshiro@mobitype.com.br>
2021-04-29 23:12:56 -0300

    Finish module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+const { request } = require('express');
 const express = require('express');
 
 const app = express();
+
+app.use(express.json());
 
 /**
  * GET - Buscar uma informação dentro do servidor
@@ -10,15 +13,29 @@ const app = express();
  * DELETE - Deletar uma informação no servidor
  */
 
+/**
+ * Tipos de parâmetros
+ * 
+ * Route Params => Identificar um recurso editar/deletar/buscar
+ * Query Params => Paginação / Filtro
+ * Body Params => Os objetos inserção/alteração (JSON)
+ */
+
 app.get('/courses', (req, res) => {
+   const query = req.query;
+   console.log(query);
    return res.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.post('/courses', (req, res) => {
+   const body = req.body;
+   console.log(body);
    return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put('/courses/:id', (req, res) => {
+   const params = req.params;
+   console.log(params);
    return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
